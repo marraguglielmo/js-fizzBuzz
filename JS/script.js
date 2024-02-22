9d6c599a7db95c2c45aef2f6a4af0190f17b74a5
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-22 17:57:39 +0100

    [add] box

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,25 @@
 const limit = 100;
+const container = document.querySelector(".container");
 
 for(let i = 1; i <= limit; i++){
+    
     console.log(i);
-
+    
     if(i % 3 === 0){
-        console.log("fizz");
+        let result = "Fizz";
+        let box = document.querySelector(".box");
+        box.innerHTML = result;
     }
     if(i % 5 === 0){
-        console.log("Buzz");
+        let result = "Buzz";
+        console.log(result);
     }
     if(i % 3 === 0 & i % 5 === 0){
-        console.log("FizzBuzz");
+        let result = "FizzBuzz";
+        console.log(result);
     }
+    
+    container.innerHTML += `
+    <div class="box">${i}</div>
+    `
 }
