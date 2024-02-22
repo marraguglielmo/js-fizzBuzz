d56089bccac6a51e1b79a545e848c2c1b8bd36f8
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-22 16:18:55 +0100

    [add] if

--- JS/script.js
+++ JS/script.js
@@ -2,4 +2,14 @@ const limit = 100;
 
 for(let i = 1; i <= limit; i++){
     console.log(i);
+
+    if(i % 3 === 0){
+        console.log("fizz");
+    }
+    if(i % 5 === 0){
+        console.log("Buzz");
+    }
+    if(i % 3 === 0 & i % 5 === 0){
+        console.log("FizzBuzz");
+    }
 }
