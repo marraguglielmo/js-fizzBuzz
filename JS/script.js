fc4118fc4c7753c5d24b50aab45912a8715f4a54
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-22 18:27:58 +0100

    [add] box

--- JS/script.js
+++ JS/script.js
@@ -1,42 +1,26 @@
-// const limit = 100;
-// const container = document.querySelector(".container");
-
-// for(let i = 1; i <= limit; i++){
-    
-//     console.log(i);
-    
-//     if(i % 3 === 0){
-//         let result = "Fizz";
-//         let box = document.querySelector(".box");
-//         box.innerHTML = result;
-//     }
-//     if(i % 5 === 0){
-//         let result = "Buzz";
-//         console.log(result);
-//     }
-//     if(i % 3 === 0 & i % 5 === 0){
-//         let result = "FizzBuzz";
-//         console.log(result);
-//     }
-    
-//     container.innerHTML += `
-//     <div class="box">${i}</div>
-//     `
-// }
-
-
 const limit = 100;
+const container = document.querySelector(".container");
 
-for(let i = 0; i < limit; i++){
+for(let i = 1; i <= limit; i++){
+    // creo il box
+    const box = document.createElement('div');
+    // assegno le classi al box
+    box.className = "box"
+    // stampo i dentro al div
+    box.innerHTML = i;
+    // metto il box nel container
+    container.append(box);
 
-    if(i % 3 === 0){
-        console.log("Fizz");
+    
+
+    if(i % 15 === 0){
+        box.innerHTML = "FizzBuzz";
     }else if(i % 5 == 0){
-        console.log("Buzz");
-    }else if(i % 15 === 0){
-        console.log("FizzBuzz");
+        box.innerHTML = "Buzz";
+    }else if(i % 3 === 0){
+        box.innerHTML = "Fizz";
     }else{
-        console.log(i);
+        box.innerHTML = i;
     }
     
 }
